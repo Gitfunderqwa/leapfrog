6f6b31b3ac5f861e379eef25400f5fb6ed228c66
Funderqwa <87319711+Gitfunderqwa@users.noreply.github.com>
2021-10-24 10:32:52 +0200

    demo build v1.2

--- src/Components/inputs/input.js
+++ src/Components/inputs/input.js
@@ -14,7 +14,9 @@ class Input extends React.Component {
             SbMinVal: 0,
             SbMaxVal: 0, 
             FNBVal: 0,
+            historyPuchaseDate: 0,
         }
+
         this.buildPDF = this.buildPDF.bind(this)
         this.handleNameChange = this.handleNameChange.bind(this)
         this.handleSqrMeterChange = this.handleSqrMeterChange.bind(this)
@@ -26,6 +28,7 @@ class Input extends React.Component {
         this.handleSbMinValChange = this.handleSbMinValChange.bind(this)
         this.handleSbMaxValChange = this.handleSbMaxValChange.bind(this)
         this.handleFNBValChange = this.handleFNBValChange.bind(this)
+        this.handlehistoryPuchaseDateChange = this.handlehistoryPuchaseDateChange.bind(this)
     }
 
     buildPDF() {
@@ -56,6 +59,10 @@ class Input extends React.Component {
         this.setState({historyPuchase : e.target.value})
     }
 
+    handlehistoryPuchaseDateChange(e) {
+        this.setState({historyPuchaseDate : e.target.value})
+    }
+
     handleSbMinValChange(e) {
         this.setState({SbMinVal : e.target.value})
     }
@@ -72,10 +79,12 @@ class Input extends React.Component {
         return (
             <div className='container'>
                 <div className ='flex container'>
+                    <form required>
                     <div className='input'>
                         <h2>Basic House Information</h2>
 
-                        <select name="property" id="propertyType" placeholder='Property Type' onChange={this.handleNameChange}>
+                        <select name="property" id="propertyType" onChange={this.handleNameChange} required="required">
+                            <option value="">Property Type</option>
                             <option value="house">House</option>
                             <option value="complex">Complex</option>
                             <option value="townhouse">Townhouse</option>
@@ -84,18 +93,21 @@ class Input extends React.Component {
                             <option value="commercial property">Commercial Property</option>
                             <option value="industrial property">Industrial Property</option>
                         </select>
-                        <input placeHolder="Square Meters" onChange={this.handleSqrMeterChange} type='number'/>
-                        <input placeHolder="Average Price per m²" onChange={this.handleAvgSqrMeterPriceChange} type='number'/>
-                        <input placeHolder="Growth Rate" onChange={this.handleRateChange} type='number'/>
-                        <input placeHolder="Address" onChange={this.handleAddressChange} type='text'/>
+                        <input placeHolder="Square Meters" onChange={this.handleSqrMeterChange} type='number' required="required"/>
+                        <input placeHolder="Average Price per m²" onChange={this.handleAvgSqrMeterPriceChange} type='number' required="required"/>
+                        <input placeHolder="Growth Rate" onChange={this.handleRateChange} type='number' required="required"/>
+                        <input placeHolder="Last Sale Date" onChange={this.handlehistoryPuchaseChange} type='number' required="required"/>
+                        <input placeHolder="Last Sale Price" onChange={this.handlehistoryPuchaseChange} type='number' required="required"/>
+                        <input placeHolder="Address" onChange={this.handleAddressChange} type='text' required="required"/>
                     </div>
                     <div className='input'>   
                         <h2>Bank Valuation</h2>
-                        <input placeHolder="Standard Bank Minimum" onChange={this.handleSbMinValChange} type='number'/>
-                        <input placeHolder="Standard Bank Maximun" onChange={this.handleSbMaxValChange} type='number'/>
-                        <input placeHolder="First National Bank Valuation" onChange={this.handleFNBValChange} type='number'/>
-                        <button className='inputButton' onClick={this.buildPDF}>Create Document</button>
+                        <input placeHolder="Standard Bank Minimum" pattern="^\R?([0-9]{1,3} ([0-9]{3} )*[0-9]{3}|[0-9]+)(,[0-9][0-9])?$" onChange={this.handleSbMinValChange} type='number' required="required"/>
+                        <input placeHolder="Standard Bank Maximun" onChange={this.handleSbMaxValChange} type='number' required="required"/>
+                        <input placeHolder="First National Bank Valuation" onChange={this.handleFNBValChange} type='number' required="required"/>
+                        <button className='inputButton' type='submit' onClick={this.buildPDF}>Create Document</button>
                     </div>
+                    </form>
                 </div>
                 
             </div>
